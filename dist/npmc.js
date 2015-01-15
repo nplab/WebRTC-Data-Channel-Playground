d75fe61dd98a9c36fe9c6825c32483d2b7f510c9
Felix Weinrank <weinrank@fh-muenster.de>
2015-01-15 15:24:53 +0100

    npmc

--- dist/npmc.js
+++ dist/npmc.js
@@ -1,39 +1,39 @@
 var npmControlChannelCounter = 1;
 
 
-$('#npmControl button[name="toggleActive"]').click(function(){
-	//alert("click");
+$('#npmControl button[name="toggleActive"]').click(function(event){
 	$(this).toggleClass('btn-default btn-primary');
 	if($(this).hasClass('btn-primary')) {
 		$(this).data('active',true);
-		console.log('aktiviert');
 	} else {
 		$(this).data('active',false);
-		console.log('deaktiviert');
 	}
-	
+	event.preventDefault();
 });
 
-$('#npmControl ul.reliabilitySelect a').click(function(){
-	
-	var parentId = $(this).closest('tr').prop('id');
-	$('#'+parentId+' .dropdown-toggle').html($(this).data('method') + ' <span class="caret"></span>');
+$('#npmControl ul.reliabilitySelect a').click(function(event){
+	var parentId 		= $(this).closest('tr').prop('id');
+	var toggleButton 	= $('#'+parentId+' .dropdown-toggle');
+	toggleButton.html($(this).data('method') + ' <span class="caret"></span>');
+	toggleButton.data('method',$(this).data('method'));
 	
 	var relInput = $('#'+parentId +' input[name=paramReliable]');	
 	console.log(parentId + ' ' + relInput);
 	
-	if($(this).data('method') == "default"){
+	if($(this).data('method') == "reliable"){
 		relInput.prop('disabled',true);
 	} else {
 		relInput.prop('disabled',false);
 	}
 	console.log('Methode gewählt:' + $(this).data('method'));
+	
+	event.preventDefault();
 });
 
 
 function addNpmControlRow() {
 	npmControlChannelCounter++;
-	var cloneRow = $('#npmControl tr.tr_clone').clone();
+	var cloneRow = $('#npmControl tr.tr_clone').clone(true);
 	cloneRow.removeClass('tr_clone');
 	cloneRow.prop('id','npmControlC'+npmControlChannelCounter);
 	cloneRow.find('[name=toggleActive]').html(npmControlChannelCounter);
@@ -42,7 +42,8 @@ function addNpmControlRow() {
 }
 
 
-$('#npmControlAddChannel').click(function(){
+$('#npmControlAddChannel').click(function(event){
 	console.log('adding row');
 	addNpmControlRow();
+	event.preventDefault();
 });
