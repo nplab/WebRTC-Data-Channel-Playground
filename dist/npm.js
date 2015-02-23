ce67351ae72b54468de6ea598492e8fa4ff17dc8
Felix Weinrank <weinrank@fh-muenster.de>
2015-02-23 11:43:40 +0100

    fix compatibility issues with firefox (urls -> url)

--- dist/npm.js
+++ dist/npm.js
@@ -59,15 +59,15 @@ var chartOptions = {
 // ICE, STUN, TURN Servers
 var iceServer = {
 	iceServers : [{
-		urls : 'stun:stun.l.google.com:19302'
+		url : 'stun:stun.l.google.com:19302'
 	}, {
-		urls : 'stun:stun1.l.google.com:19302'
+		url : 'stun:stun1.l.google.com:19302'
 	}, {
-		urls : 'stun:stun2.l.google.com:19302'
+		url : 'stun:stun2.l.google.com:19302'
 	}, {
-		urls : 'stun:stun3.l.google.com:19302'
+		url : 'stun:stun3.l.google.com:19302'
 	}, {
-		urls : 'stun:stun4.l.google.com:19302'
+		url : 'stun:stun4.l.google.com:19302'
 	}]
 };
 
