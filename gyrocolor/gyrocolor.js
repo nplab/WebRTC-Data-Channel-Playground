9e8a78c796eb9b75353f51ea39fa470c6ad9546c
Felix Weinrank <info@weinrank.net>
2015-07-18 15:19:18 +0200

    refactoring

--- gyrocolor/gyrocolor.js
+++ gyrocolor/gyrocolor.js
@@ -1,34 +1,33 @@
 /*-
- * Copyright (c) 2015 Felix Weinrank
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- */
+* Copyright (c) 2015 Felix Weinrank
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+*
+*/
 
 /*
- BASED ON: http://louisstow.github.io/WebRTC/datachannels.html
- */
-
+BASED ON: http://louisstow.github.io/WebRTC/datachannels.html
+*/
 
 // ICE, STUN, TURN Servers
 var iceServer = {
@@ -57,58 +56,24 @@ var sdpConstraints = {
 var dbRef = new Firebase("https://webrtc-data-channel.firebaseio.com/");
 
 // shims - wrappers for webkit and mozilla connections
-var PeerConnection 		= window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-var IceCandidate 		= window.mozRTCIceCandidate || window.RTCIceCandidate;
-var SessionDescription 	= window.RTCSessionDescription || window.mozRTCSessionDescription || window.RTCSessionDescription;
+var PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.RTCSessionDescription;
 
-var offerer = false;
-var parameters = {};
+var bufferedAmountLimit = 1 * 1024 * 1024;
+
 var pc = new PeerConnection(iceServer);
 var peerRole = "offerer";
 var role = "answerer";
 var signalingId;
 var freshsignalingId = generateSignalingId();
 var signalingIdRef = dbRef.child("gyroIDs");
-var t_startNewPackage = 0;
 var dcControl = {};
+var gyroColorFromRemote = false;
 
 
-function gyroInit() {
-	//if (window.DeviceOrientationEvent  && 'ontouchstart' in window) {
-		if (window.DeviceOrientationEvent) {
-		$('#gyrostatus').removeClass('alert-danger').addClass('alert-success');
-		// Listen for the deviceorientation event and handle the raw data
-		window.addEventListener('deviceorientation', function(eventData) {
-			// gamma is the left-to-right tilt in degrees, where right is positive
-			
-			var gammaRaw = Math.round(event.gamma);
-			var gamma = Math.round((Math.abs(eventData.gamma) * 2.83) % 255);
+var speedtestInitator = false;
 
-			// beta is the front-to-back tilt in degrees, where front is positive
-			var betaRaw = Math.round(event.beta);
-			var beta = Math.round((Math.abs(eventData.beta) *1.41 )% 255);
-
-			// alpha is the compass direction the device is facing in degrees
-			var alphaRaw = Math.round(event.alpha);
-			var alpha = Math.round((Math.abs(eventData.alpha-180) / 0.7)% 255);
-			
-			if(gammaRaw != 0 && betaRaw != 0 && alphaRaw != 0) {
-				$('#gyrostatus').html('alpha:' + alpha + ' beta:' + beta + ' gamma:' + gamma + '<br />' + 'alpha:' + alphaRaw + ' beta:' + betaRaw + ' gamma:' + gammaRaw );
-				
-				
-				//$(document.body).css('background-color','rgb('+alpha+','+beta+','+gamma+')');​​​​​​​​​​​​​​​
-				$('body').css('background-color','rgb('+alpha+','+beta+','+gamma+')');
-				
-				if(typeof(dcControl.readyState) !== 'undefined' && dcControl.readyState === "open") {
-					//alert('sending');
-					dcControl.send(alpha+','+beta+','+gamma);
-				}
-			}
-			
-			// call our orientation event handler
-		}, false);
-	}
-}
 
 // clean firebase ref
 signalingIdRef.child(freshsignalingId).remove();
@@ -140,10 +105,6 @@ function errorHandler(err) {
 	console.error(err);
 }
 
-pc.oniceconnectionstatechange = function(event) { 
-	console.log("oniceconnectionstatechange - " + pc.iceConnectionState);
-};
-
 // handle local ice candidates
 pc.onicecandidate = function(event) {
 	// take the first candidate that isn't null
@@ -153,55 +114,68 @@ pc.onicecandidate = function(event) {
 
 	var ip = extractIpFromString(event.candidate.candidate);
 
-
 	// add local ice candidate to firebase
 	signalingIdRef.child(signalingId).child(role + '-iceCandidates').push(JSON.stringify(event.candidate));
 
 	console.log('onicecandidate - ip:' + ip);
 };
 
+pc.oniceconnectionstatechange = function(event) { 
+	console.log("oniceconnectionstatechange - " + pc.iceConnectionState);
+	if (pc.iceConnectionState === 'disconnected') {
+		speedtestConnectionLost();
+	}
+};
+
 function gyroCreateSignalingId() {
+	console.log('gyroCreateSignalingId');
 	signalingId = freshsignalingId;
-	offerer = true;
 	role = "offerer";
 	peerRole = "answerer";
-	
-	$('#connectionStatus').text('waiting for peer - connect with to id ' + signalingId);
+
 	console.log('creating signaling id:' + signalingId);
 	gyroConnect();
 }
 
-
 function gyroConnectTosignalingId() {
+	console.log('gyroConnectTosignalingId');
 	signalingId = $("#signalingId").val();
-	offerer = false;
 	role = "answerer";
 	peerRole = "offerer";
-	
-	$('#connectionStatus').text('connecting to peer id ' + signalingId);
+
 	console.log('connecting to peer:' + signalingId);
 	gyroConnect();
-	
+
 }
 
+function gyroConnectTosignalingIdFromUrl() {
+	console.log('gyroConnectTosignalingId');
+	role = "answerer";
+	peerRole = "offerer";
+
+	console.log('connecting to peer:' + signalingId);
+	gyroConnect();
+
+} 
 
 // establish connection to remote peer via webrtc
 function gyroConnect() {
 
-	// disable inputs
-	//$("#colCreateSignalingId").hide();
-	$("#colCreateSignalingId").fadeOut();
-	$("#colConnectToSignalingId").fadeOut(400,function(){
-		$("#colConnectionStatus").removeClass("hidden");
-	});
-	
+	$("#rowInit").slideUp();
+	$("#rowSpinner").slideDown();
 
 	if (role === "offerer") {
+		$(".spinnerStatus").html('waiting for peer<br/>use id: ' + signalingId + '<br/><br/><div id="qrcode"></div>');
+		
+		new QRCode(document.getElementById("qrcode"), window.location.href + '#' + signalingId);
+		
+		$("#rowSpinner").removeClass('hidden').hide().slideDown();
 
 		dcControl = pc.createDataChannel('control');
+
+		bindEventsControl(dcControl);
 		
-		bindEvents(dcControl);
-		
+
 		// create the offer SDP
 		pc.createOffer(function(offer) {
 			pc.setLocalDescription(offer);
@@ -219,12 +193,17 @@ function gyroConnect() {
 
 		// answerer role
 	} else {
-
+		$(".spinnerStatus").text("connecting - id: " + signalingId);
+		$("#rowSpinner").removeClass('hidden').hide().slideDown();
 		// answerer must wait for the data channel
 		pc.ondatachannel = function(event) {
-			dcControl = event.channel;
-			console.log(event);
-			bindEvents(dcControl);
+			if (event.channel.label == "control") {
+				dcControl = event.channel;
+				bindEventsControl(event.channel);
+			} else {
+				alert("error: unknown channel!");
+			}
+
 			console.log('incoming datachannel');
 		};
 
@@ -252,27 +231,31 @@ function gyroConnect() {
 		pc.addIceCandidate(new IceCandidate(peerCandidate));
 
 		var peerIp = extractIpFromString(peerIceCandidate.candidate);
-		
-		
+
 		console.log('peerIceCandidate: ' + peerIp);
 	});
 }
 
 // find and return an IPv4 Address from a given string
 function extractIpFromString(string) {
+	console.log(string);
 	var pattern = '(?:25[0-5]|2[0-4][0-9]|1?[0-9][0-9]{1,2}|[0-9]){1,}(?:\\.(?:25[0-5]|2[0-4][0-9]|1?[0-9]{1,2}|0)){3}';
 	var match = string.match(pattern);
 	return match[0];
 }
 
-// bind the channel events
-function bindEvents(channel) {
+// bind events for control channel
+function bindEventsControl(channel) {
 	channel.onopen = function() {
+		$("#rowSpinner").slideUp();
+		$("#rowControl").slideDown();
 		console.log("Channel Open - Label:" + channel.label + ', ID:' + channel.id);
 	};
 
 	channel.onclose = function(e) {
 		console.log("Channel Close");
+		gyroConnectionLost();
+
 	};
 
 	window.onbeforeunload = function() {
@@ -280,10 +263,96 @@ function bindEvents(channel) {
 	};
 
 	channel.onmessage = function(e) {
-		rxData = e.data.toString();
-		$('body').css('background-color','rgb('+rxData+')');
-		console.log("Message for "+e.currentTarget.label+" - content:"+rxData);
+		msgHandleJson(e.data.toString());
 	};
 }
 
+
+function gyroConnectionLost() {
+	speedtestContinueSending = false;
+	$("#rowSpinner").hide();
+	$("#rowMessage").removeClass('hidden');
+	$("#colMessage").html('<div class="alert alert-danger text-center" role="alert"><strong>Error:</strong> Connection to peer lost!</div>');
+
+}
+
+function gyroInit() {
+	//if (window.DeviceOrientationEvent  && 'ontouchstart' in window) {
+	
+	if(location.hash.substring(1)){
+		signalingId = location.hash.substring(1);
+		gyroConnectTosignalingIdFromUrl();
+	}
+       
+	if (window.DeviceOrientationEvent) {
+		console.log("hallo");
+		$('#gyrostatus').removeClass('alert-info').addClass('alert-success');
+		// Listen for the deviceorientation event and handle the raw data
+		window.addEventListener('deviceorientation', function(eventData) {
+			console.log("hallo2");
+			// gamma is the left-to-right tilt in degrees, where right is positive
+			var gammaRaw = Math.round(event.gamma);
+			var gamma = Math.round((Math.abs(eventData.gamma) * 2.83) % 255);
+
+			// beta is the front-to-back tilt in degrees, where front is positive
+			var betaRaw = Math.round(event.beta);
+			var beta = Math.round((Math.abs(eventData.beta) *1.41 )% 255);
+
+			// alpha is the compass direction the device is facing in degrees
+			var alphaRaw = Math.round(event.alpha);
+			var alpha = Math.round((Math.abs(eventData.alpha-180) / 0.7)% 255);
+			
+			if(gammaRaw != 0 && betaRaw != 0 && alphaRaw != 0) {
+				$('#gyrostatus').html('alpha:' + alpha + ' beta:' + beta + ' gamma:' + gamma + '<br />' + 'alpha:' + alphaRaw + ' beta:' + betaRaw + ' gamma:' + gammaRaw );
+				
+				if(!gyroColorFromRemote) {
+					gyroSetColor(alpha,beta,gamma);
+				}
+				
+				if(typeof(dcControl.readyState) !== 'undefined' && dcControl.readyState === "open") {
+					//alert('sending');
+
+					var gyroData = {
+						type : 'gyro',
+						alpha : alpha,
+						beta : beta,
+						gamma : gamma
+					}
+
+					dcControl.send(JSON.stringify(gyroData));
+				}
+			}
+			
+			// call our orientation event handler
+		}, false);
+	}
+}
+
+function gyroSetColor(alpha, beta, gamma) {
+	console.log('color: ' + alpha + ' - ' + beta + ' - gamma ' + gamma);
+	$('body').css('background-color','rgb('+alpha+','+beta+','+gamma+')');
+    $('#complementary').css('color','rgb('+(alpha+128)%255+','+(beta+128)%255+','+(gamma+128)%255+')' );
+}
+
+
+function msgHandleJson(message) {
+	var messageObject = JSON.parse(message);
+
+	console.log('message!');
+
+	switch(messageObject.type) {
+	
+	// peer indicates finish
+	case 'gyro':
+		gyroColorFromRemote = true;
+		console.log(message);
+		gyroSetColor(messageObject.alpha,messageObject.beta,messageObject.gamma);
+	break;
+
+
+	default:
+		alert('Unknown messagetype: ' + messageObject.type);
+		break;
+	}
+}
 
