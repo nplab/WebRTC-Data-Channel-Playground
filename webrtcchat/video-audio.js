1ec5a132c15f84850f7bc5ee7d437952ab71f7c7
Viktor Schlegel <me001000@yahoo.de>
2015-10-30 15:06:47 +0100

    development

--- webrtcchat/video-audio.js
+++ webrtcchat/video-audio.js
@@ -38,17 +38,17 @@ document.getElementById("enteruser").style.display = "none";
 
 navigator.getMedia = navigator.getUserMedia|| navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
+var localstream;
 var user = 0;
+var i = 0;
+var x = 0;
 var peer1ID;
 var localwebcam = document.getElementById("local");
-var remotewebcam = document.getElementById("remote");
-var remotewebcam2 = document.getElementById("2ndremote");
+
 navigator.getMedia(sdpConstraints, function(stream) {
 	localwebcam.src = URL.createObjectURL(stream);
 	localwebcam.muted = true;
 	localstream = stream;
-	pc1.addStream(localstream);
-	pc2.addStream(localstream);
 }, errorHandler);	
 
 // Reference to Firebase APP
@@ -63,16 +63,19 @@ document.getElementById("download").style.display = "none";
 document.getElementById("upload").style.display = "none";
 document.getElementById("sendfile").style.display = "none";  	
 var arrayToStoreChunks = [];
-var pc1 = new PeerConnection(iceServer);
-var pc2 = new PeerConnection(iceServer);
+var pc = new Array();
+pc[0] = new PeerConnection(iceServer);
 var peerRole = "offerer";
 var role = "answerer";
 var signalingId;
 var freshsignalingId = generateSignalingId();
 var signalingIdRef = dbRef.child("roomIDs");
-var dcControl1 = {};
-var dcControl2 = {};
+var peerIp = new Array();
+var peerID = new Array();
+var dcControl = new Array();
+dcControl[0] = {};
 var zaehler = 0;
+var dVideos = $('#dVideos'); 
 
 // clean firebase ref
 signalingIdRef.child(freshsignalingId).remove();
@@ -99,52 +102,9 @@ function errorHandler(err) {
 	console.error(err);
 }
 
-// handle local ice candidates
-pc1.onicecandidate = function(event) {
-	// take the first candidate that isn't null
-	if (!pc1 || !event || !event.candidate) {
-		return;
-	}
-
-	var ip = extractIpFromString(event.candidate.candidate);
-
-	// add local ice candidate to firebase
-	signalingIdRef.child(signalingId).child(role + '-iceCandidates').push(JSON.stringify(event.candidate));
-
-	console.log('onicecandidate - ip:' + ip);
-};
-
-pc2.onicecandidate = function(event) {
-	// take the first candidate that isn't null
-	if (!pc2 || !event || !event.candidate) {
-		return;
-	}
-
-	var ip = extractIpFromString(event.candidate.candidate);
-
-	// add local ice candidate to firebase
-	signalingIdRef.child(signalingId).child(role + '-iceCandidates').push(JSON.stringify(event.candidate));
-
-	console.log('onicecandidate - ip:' + ip);
-};
-
-pc1.oniceconnectionstatechange = function(event) { 
-	console.log("oniceconnectionstatechange1 - " + pc1.iceConnectionState);
-
-	if (pc1.iceConnectionState == 'disconnected') {
-		chatConnectionLost();
-	}
-};
-pc2.oniceconnectionstatechange = function(event) { 
-	console.log("oniceconnectionstatechange2 - " + pc2.iceConnectionState);
-	
-	if (pc2.iceConnectionState === 'disconnected') {
-		chatConnectionLost();
-	}
-};
-
-
 function chatCreateSignalingId() {
+	i++;
+	freshsignalingId = generateSignalingId();
 	console.log('chatCreateSignalingId');
 	signalingId = freshsignalingId;
 	role = "offerer";
@@ -155,6 +115,7 @@ function chatCreateSignalingId() {
 }
 
 function chatConnectTosignalingId() {
+	i++;
 	console.log('chatConnectTosignalingId');
 	signalingId = $("#signalingId").val();
 	role = "answerer";
@@ -174,53 +135,66 @@ function chatConnectTosignalingIdFromUrl() {
 } 
 
 function chatConnect() {
+	dcControl[i] = {};
+	pc[i] = new PeerConnection(iceServer);
+	pc[i].addStream(localstream);
+	pc[i].oniceconnectionstatechange = function(event) 
+	{ 
+		console.log("oniceconnectionstatechange1 - " + pc[i].iceConnectionState);
+	
+		if (pc[i].iceConnectionState == 'disconnected') {
+			chatConnectionLost();
+		}
+	};
+	
+	pc[i].onaddstream = function (obj) 
+	{
+		console.log("got stream");
+		user++;
+		var video = document.createElement('video');
+		dVideos.append("<video id='v" + user + "' height='400px' width='400px' src='" + URL.createObjectURL(obj.stream)+"' autoplay>");
+	};
+	
+	// handle local ice candidates
+	pc[i].onicecandidate = function(event) 
+	{
+		// take the first candidate that isn't null
+		if (!pc[i] || !event || !event.candidate) {
+			return;
+		}
+		var ip = extractIpFromString(event.candidate.candidate);
+		// add local ice candidate to firebase
+		signalingIdRef.child(signalingId).child(role + '-iceCandidates').push(JSON.stringify(event.candidate));
+		console.log('onicecandidate - ip:' + ip);
+	};
+	
 	if (role === "offerer") {
 	document.getElementById("ausgabe").value = signalingId;
-		dcControl1 = pc1.createDataChannel('control');
-		bindEventsControl(dcControl1);
-		dcControl2 = pc2.createDataChannel('control');
-		bindEventsControl(dcControl2);
-		if(zaehler === 0)
-		{
-			// create the offer SDP
-			pc1.createOffer(function(offer) {
-			pc1.setLocalDescription(offer);
-
-			// send the offer SDP to FireBase
-			firebaseSend(signalingId, "offer", JSON.stringify(offer));
-
-			// wait for an answer SDP from FireBase
-			firebaseReceive(signalingId, "answer", function(answer) {
-				pc1.setRemoteDescription(new SessionDescription(JSON.parse(answer)));
-			});
-		}, errorHandler, sdpConstraints);
-		console.log("connect - role: offerer");
-		}
-		else
-		{
-			pc2.createOffer(function(offer) {
-			pc2.setLocalDescription(offer);
-
-			// send the offer SDP to FireBase
-			firebaseSend(signalingId, "offer", JSON.stringify(offer));
-
-			// wait for an answer SDP from FireBase
-			firebaseReceive(signalingId, "answer", function(answer) {
-				pc2.setRemoteDescription(new SessionDescription(JSON.parse(answer)));
-			});
-		}, errorHandler, sdpConstraints);
-		console.log("connect - role: offerer");
-		}
+		dcControl[i] = pc[i].createDataChannel('control');
+		bindEventsControl(dcControl[i]);
 		
+		
+			// create the offer SDP
+			pc[i].createOffer(function(offer) {
+			pc[i].setLocalDescription(offer);
 
+			// send the offer SDP to FireBase
+			firebaseSend(signalingId, "offer", JSON.stringify(offer));
+
+			// wait for an answer SDP from FireBase
+			firebaseReceive(signalingId, "answer", function(answer) {
+				pc[i].setRemoteDescription(new SessionDescription(JSON.parse(answer)));
+			});
+		}, errorHandler, sdpConstraints);
+		console.log("connect - role: offerer");
+		document.getElementById("IDS").value = document.getElementById("IDS").value + signalingId + "\r\n";
 		// answerer role
 	} else {
-		if(zaehler === 0)
-		{
+		
 			// answerer must wait for the data channel
-		pc1.ondatachannel = function(event) {
+		pc[i].ondatachannel = function(event) {
 			if (event.channel.label == "control") {
-				dcControl1 = event.channel;
+				dcControl[i] = event.channel;
 				bindEventsControl(event.channel);
 			} else {
 				alert("error: unknown channel!");
@@ -231,68 +205,26 @@ function chatConnect() {
 		
 		// answerer needs to wait for an offer before generating the answer SDP
 		firebaseReceive(signalingId, "offer", function(offer) {
-			pc1.setRemoteDescription(new SessionDescription(JSON.parse(offer)));
-
+			pc[i].setRemoteDescription(new SessionDescription(JSON.parse(offer)));
+			
 			// now we can generate our answer SDP
-			pc1.createAnswer(function(answer) {
-				pc1.setLocalDescription(answer);
+			pc[i].createAnswer(function(answer) {
+				pc[i].setLocalDescription(answer);
 
 				// send it to FireBase
 				firebaseSend(signalingId, "answer", JSON.stringify(answer));
 			}, errorHandler);
 		});
 		// add handler for peers ice candidates
-		signalingIdRef.child(signalingId).child(peerRole + '-iceCandidates').on('child_added', function(childSnapshot) {
+			signalingIdRef.child(signalingId).child(peerRole + '-iceCandidates').on('child_added', function(childSnapshot) {
 			var childVal = childSnapshot.val();
 			var peerCandidate = JSON.parse(childVal);
 			var peerIceCandidate = new IceCandidate(peerCandidate);
-			pc1.addIceCandidate(new IceCandidate(peerCandidate));
+			pc[i].addIceCandidate(new IceCandidate(peerCandidate));
 	
-			var peerIp1 = extractIpFromString(peerIceCandidate.candidate);
-			console.log('peerIceCandidate for pc1: ' + peerIp1);
-			
+			peerIp[i] = extractIpFromString(peerIceCandidate.candidate);
+			console.log('peerIceCandidate for pc: ' + peerIp[i]);
 		});
-		}
-		else
-		{
-			pc2.ondatachannel = function(event) {
-			if (event.channel.label == "control") {
-				dcControl2 = event.channel;
-				bindEventsControl(event.channel);
-			} else {
-				alert("error: unknown channel!");
-			}
-
-			console.log('incoming datachannel');
-		};
-		
-		
-		firebaseReceive(signalingId, "offer", function(offer) {
-			pc2.setRemoteDescription(new SessionDescription(JSON.parse(offer)));
-
-			// now we can generate our answer SDP
-			pc2.createAnswer(function(answer) {
-				pc2.setLocalDescription(answer);
-
-				// send it to FireBase
-				firebaseSend(signalingId, "answer", JSON.stringify(answer));
-			}, errorHandler);
-		});
-		console.log('connect - role answerer');
-		console.log('connect - role answerer');
-		signalingIdRef.child(signalingId).child(peerRole + '-iceCandidates').on('child_added', function(childSnapshot) {
-			var childVal = childSnapshot.val();
-			var peerCandidate = JSON.parse(childVal);
-	
-			var peerIceCandidate = new IceCandidate(peerCandidate);
-			pc2.addIceCandidate(new IceCandidate(peerCandidate));
-			
-			var peerIp2 = extractIpFromString(peerIceCandidate.candidate);
-			console.log('peerIceCandidate for pc2: ' + peerIp2);
-	
-			
-		});
-		}
 	}
 }
 
@@ -310,26 +242,11 @@ function bindEventsControl(channel) {
 		zaehler++;
 		if(zaehler == 1)
 		{
+			//document.getElementById("IDS").value = document.getElementById("IDS").value + signalingId + ": " + dcControl[i].readyState + "\r\n";
 			document.getElementById("enteruser").style.display = "block";
 			document.getElementById("upload").style.display = "block";	
 			document.getElementById("sendfile").style.display = "block"; 
 		}
-		if(zaehler == 2)
-		{
-			var createsignal = {
-    		type : 'createsignal'
-   			 }; 
-   			 dcControl1.send(JSON.stringify(createsignal));
-		}
-		if(role != "answerer" && zaehler == 1)
-		{
-			remoteCreateID();
-		}
-		if(dcControl2.readyState == 'open' && role != "answerer" && connected == 0)
-		{
-			sendRemoteConnect(peer1ID);
-		}
-		freshsignalingId = generateSignalingId();
 		console.log("Channel Open - Label:" + channel.label + ', ID:' + channel.id); 
 	};
 
@@ -350,24 +267,6 @@ function bindEventsControl(channel) {
 
 function chatConnectionLost() {
 	console.log("Connection lost");
-}
-	
-function sendmessage()
-{
-	var eingabemsg = document.getElementById("eingabe").value;
-	document.getElementById("chatarea").value = document.getElementById("chatarea").value + eingabemsg + "\r\n";
-	nachricht = document.getElementById("chatarea").value;
-	
-	var peermsg = {
-						type : 'msg',
-						nachricht : nachricht 
-					};
-	dcControl1.send(JSON.stringify(peermsg));
-	if(user == 2)
-   	{
-   		dcControl2.send(JSON.stringify(peermsg));
-   	} 
-	chatarea.scrollTop = chatarea.scrollHeight;
 }
 
 function setmessage(nachricht)
@@ -408,29 +307,6 @@ function msgHandleJson(message) {
 		 setfile(messageObject);
 	break;
 	
-	case 'createsignal' :
-		 createsignal();
-	break;
-
-	
-	case 'ID' :
-		connected++;
-		console.log('chatCreateSignalingId');
-		signalingId = freshsignalingId;
-		console.log('creating signaling id:' + signalingId);
-		sendRemoteID();
-	break;
-	
-	case 'GotID' :
-		console.log("got peer1 ID");
-		peer1ID = messageObject.signalingId;
-	break;
-	
-	case 'RConnect' :
-		document.getElementById("signalingId").value = messageObject.peer1ID;
-		chatConnectTosignalingId();
-	break;
-	
 	default:
 		alert('Unknown messagetype: ' + messageObject.type);
 		break;
@@ -466,28 +342,14 @@ $('#eingabe').keypress(function (e) {
 						};
 						
 						
-		dcControl1.send(JSON.stringify(peermsg));
-		if(user == 2)
-   		{
-   			dcControl2.send(JSON.stringify(peermsg));
- 	  	} 
+		for (var y = 0; y <= i; y++)
+ 		{
+ 			dcControl[i].send(JSON.stringify(peermsg));
+ 		}	
+		
 		document.getElementById("eingabe").value = "";
   	}
 });   
-
-pc1.onaddstream = function (obj) {
-	user++;
-	remotewebcam2.src = URL.createObjectURL(obj.stream);
-	remotewebcam2.play();
-	console.log("got stream");
-};
-
-pc2.onaddstream = function (obj) {
-	user++;
-	remotewebcam.src = URL.createObjectURL(obj.stream);
-	remotewebcam.play();
-	console.log("got stream");
-};
 
 function onReadAsDataURL(event, text) {
 	var filename = document.getElementById('upload').files[0].name;
@@ -495,7 +357,7 @@ function onReadAsDataURL(event, text) {
     	type : 'file',
     	filename : filename
     }; // data object to transmit over data channel
-    var chunkLength = 16385;
+    var chunkLength = 16384;
 
     if (event) text = event.target.result; // on first invocation
     if (text.length > chunkLength) {
@@ -505,11 +367,10 @@ function onReadAsDataURL(event, text) {
         data.message = text;
         data.last = true;
     }
-   	dcControl1.send(JSON.stringify(data));
-   	if(user == 2)
-   	{
-   		dcControl2.send(JSON.stringify(data));
-   	} 
+   for (var y = 0; y <= i; y++)
+ 	{
+ 		dcControl[i].send(JSON.stringify(data));
+ 	}
 
     var remainingDataURL = text.slice(data.message.length);
     if (remainingDataURL.length) setTimeout(function () {
@@ -524,39 +385,4 @@ function SaveToDisk(fileUrl, fileName) {
     save.download = fileName || fileUrl;
     save.text = "Download: " + fileName;
     document.getElementById("download").style.display = "block";
-}
-
-function remoteCreateID()
-{
-	var CreateID = {
-							type : 'ID'
-		};	
-	dcControl1.send(JSON.stringify(CreateID));
-	console.log("peer1 creates ID");
-}
-
-function sendRemoteID()
-{
-	var GotID = {
-							type : 'GotID',
-							signalingId : signalingId
-	};	
-	dcControl1.send(JSON.stringify(GotID));					
-}
-
-function sendRemoteConnect(peer1ID)
-{
-	var RemoteConnect = {
-							type : 'RConnect',
-							peer1ID : peer1ID
-	};	
-	dcControl2.send(JSON.stringify(RemoteConnect));
-	console.log("sending peer2 peer1ID");
-}
-
-function createsignal()
-{
-		role = "offerer";
-		peerRole = "answerer";
-		chatConnect();	
 }
