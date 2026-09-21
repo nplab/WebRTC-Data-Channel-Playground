aecffaf6a16f8d4da6ea6050eb2044a2f727f7bd
msvoelker <timo.voelker@fh-muenster.de>
2026-09-21 16:41:37 +0200

    Update gyrocolor.js

--- gyrocolor/gyrocolor.js
+++ gyrocolor/gyrocolor.js
@@ -25,6 +25,9 @@
 *
 */
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-database.js";
+
 /*
 BASED ON: http://louisstow.github.io/WebRTC/datachannels.html
 */
@@ -40,8 +43,6 @@ var sdpConstraints = {
 // Reference to Firebase APP
 //var dbRef = new Firebase("https://webrtc-data-channel.firebaseio.com/");
 //var dbRef = new Firebase("https://msvoelker-webrtc-default-rtdb.firebaseio.com/");
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-database.js";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
